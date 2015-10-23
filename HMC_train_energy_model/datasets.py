more scaling fixes

HMC_train_energy_model/datasets.py
@@ -106,9 +106,10 @@ def load_pixel_sparse(n_imgs=5, n_patches=100000, patch_x=4, patch_y=4):
     """
     Generate pixel-sparse training data.
     """
-    n = np.random.randn(n_patches, patch_x*patch_y)
-    patches_unnorm = n**3
-    patches = patches_unnorm / np.std(patches_unnorm)
+    #n = np.random.randn(n_patches, patch_x*patch_y)
+    #patches_unnorm = n**3
+    #patches = patches_unnorm / np.std(patches_unnorm)
+    patches = np.random.laplace(size=(n_patches, patch_x*patch_y))
     W_X = np.eye(patch_x*patch_y)
     # DEBUG why is this different from what's expected of load_van_hateren
     #return patches, W_X

HMC_train_energy_model/energies.py
@@ -35,14 +35,18 @@ class gauss_2d:
 	
     
 class ICA_soft_laplace:
-    def __init__(self):
+    def __init__(self, weight_scale=32):
+        """
+        weight_scale acts like the natural gradient. should set it to be the square root of the number of samples.
+        """
         # theta here is the receptive field J:[n_dim]* [n_expert], for ica, J is a square matrix
         self.theta = T.matrix('theta')
         self.epsilon_np  = np.array(0.1, dtype = theano.config.floatX)
         self.epsilon = theano.shared(self.epsilon_np)
         self.name = 'ICA_soft_laplace'
+        self.weight_scale = theano.shared(np.array(weight_scale, dtype=theano.config.floatX))
     def E(self, X):
-        XJ = T.dot(X, self.theta) # [n_sample]*[n_expert]
+        XJ = T.dot(X, self.theta/self.weight_scale) # [n3_sample]*[n_expert]
         XJ2_ep = self.epsilon + XJ**2
         return T.sum(T.sqrt(XJ2_ep), axis=1)
     def dE_dtheta(self, X, acpt=None):
@@ -50,18 +54,3 @@ class ICA_soft_laplace:
             return T.grad(T.mean(self.E(X)), self.theta, consider_constant=[X])
         else:
             return T.grad(T.sum(acpt*self.E(X)), self.theta, consider_constant=[X, acpt])
-    
-    
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-  

HMC_train_energy_model/test_ICA.py
@@ -33,10 +33,14 @@ stepsize_baseline = 0.2
         # stepsize_baseline = 0.1
 noise_level = 2
 stepsizes0 = stepsize_baseline*noise_level**random_interval 
+
+# DEBUG
+train_x = train_x[:n_sample]
        
 initial_v = rng.randn(n_sample, n_dim)
 
 initial_params = rng.randn(n_sample+n_dim, n_dim)
+initial_params[n_sample:] = initial_params[n_sample:] / np.sqrt(n_dim) * np.sqrt(n_sample)
 initial_params_flat = initial_params.flatten()
 
 #params_original = []
@@ -47,7 +51,7 @@ best_samples_list = scipy.optimize.fmin_l_bfgs_b(objective.f_df_wrapper,
                                     initial_params_flat,
                                     args = args_hyper,
                                     maxfun=1000,
-                                    # disp=1,
+                                    #disp=1,
                                     )
                                     
 optimal_param = best_samples_list[0]
@@ -55,17 +59,15 @@ best_samples = optimal_param[:(n_sample*n_dim)].reshape(n_sample, n_dim)
 J = optimal_param[(n_sample*n_dim):].reshape(n_dim, n_dim)
 J_inv = linalg.inv(J)
 receptive_field = utils.tile_raster_images(J, (imwidth,imwidth),(10,10), (1,1))
-image = Image.fromarray(receptive_field)
+image = Image.fromarray(receptive_field).resize((imwidth*10*8, imwidth*10*8))
 image.save('J_patches100000_step50_nsample1000.png')     
 receptive_field_inv = utils.tile_raster_images(J_inv, (imwidth,imwidth),(10,10), (1,1))
-image1 = Image.fromarray(receptive_field_inv)   
+image1 = Image.fromarray(receptive_field_inv).resize((imwidth*10*8, imwidth*10*8))
 image1.save('J-inv_patches100000_step50_nsample1000.png')     
-samples_vis = utils.tile_raster_images(best_samples, (imwidth,imwidth),(50,20), (1,1))  
-samples_vis_image = Image.fromarray(samples_vis)
+samples_vis = utils.tile_raster_images(best_samples, (imwidth,imwidth),(10,10), (1,1))  
+samples_vis_image = Image.fromarray(samples_vis).resize((imwidth*10*8, imwidth*10*8))
 samples_vis_image.save('representative samples.png')
 train_x_sub = train_x[:100, ]
 train_x_show = utils.tile_raster_images(train_x_sub, (imwidth,imwidth),(10,10), (1,1))
-image_train = Image.fromarray(train_x_show)
+image_train = Image.fromarray(train_x_show).resize((imwidth*10*8, imwidth*10*8))
 image_train.save('training_samples.png')
-          
-                 

HMC_train_energy_model/training_objective.py
@@ -36,14 +36,19 @@ def theano_funcs(theta,energy, dE_dtheta):
     # get sampler_cost
     sampler_cost = dE_dtheta(initial_pos_vec, accept_flatten) - dE_dtheta(final_pos_vec, accept_flatten)
     #sampler_cost = dE_dtheta(initial_pos_vec, accept_flatten)
-    sampler_cost = 1./(final_pos.shape[0]) * sampler_cost
-    sampler_cost = T.mean(sampler_cost**2)
+    #sampler_cost = 1./(final_pos.shape[0]) * sampler_cost
+    sampler_cost = 1./(final_pos_vec.shape[0]) * sampler_cost
+    sampler_cost = T.sum(sampler_cost**2)
     # get param_cost
     param_cost = dE_dtheta(training) - dE_dtheta(initial_pos)
-    param_cost = T.mean(param_cost**2)
-        
+    param_cost = T.sum(param_cost**2)
+
+    # DEBUG counteract scaling of theta
+    sampler_cost = sampler_cost * training.shape[0]
+    param_cost   = param_cost * training.shape[0]
+
     total_cost = param_cost + sampler_cost
-    costs = [param_cost, sampler_cost]
+    costs = [total_cost, param_cost, sampler_cost]
     gparams = []
     for param in params:
         gparam = T.grad(total_cost, param)
@@ -92,7 +97,7 @@ class training_objective:
         rval 1: param_cost and sampler_cost (list)
         rval 2: grad_sampler and grad_param (list)
         """
-        return results[:2], results[2:]
+        return results[:3], results[3:]
         
     def f_df_wrapper(self, params, *args):
         #input is the flattened version of params
@@ -114,9 +119,13 @@ class training_objective:
         f1, df1 = self.f_df(params_original, training_X, *args[1:])
         f = 0.
         df = 0.
-        f += (f1[0] + f1[1])
-        print "param_cost=", f1[0]
-        print "sampler_cost=", f1[1]
+        f += f1[0]
+        print "total_cost=%g param_cost=%g sampler_cost=%g rms_samp=%g rms_J=%g rms_dsamp=%g rms_dJ=%g"%(
+            f1[0], f1[1], f1[2],
+            np.sqrt(np.mean(params_original[0]**2)),
+            np.sqrt(np.mean(params_original[1]**2)),
+            np.sqrt(np.mean(df1[0]**2)),
+            np.sqrt(np.mean(df1[1]**2)))
         # df1 is the list, so need to convert it to flat representation
         """
         list to flat, use the handy func from SFO